b1251113322b92d96a9116eed2fa159bb387267a
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2018-04-20 17:11:24 +0900

    example追加

---
+++ examples/example.py
@@ -0,0 +1,55 @@
+from hiargparse import ArgumentParser, Namespace  # just same as argparse
+from hiargparse import ArgsProvider, Arg, ChildProvider  # need to define child arguments
+
+
+class GrandSon:
+    @classmethod
+    def get_args_provider(cls) -> ArgsProvider:
+        return ArgsProvider(
+            args=[
+                Arg('huga', None, type=str),
+                Arg('piyo', 0.99, help='%(default-text)s This argument is Piyopiyo.')
+            ]
+        )
+
+    def __init__(self, params: Namespace) -> None:
+        print(params.huga, params.piyo)
+
+
+class Son:
+    @classmethod
+    def get_args_provider(cls) -> ArgsProvider:
+        return ArgsProvider(
+            args=[
+                Arg('hoge', 42)
+            ],
+            propagate_args=[
+                Arg('huga', None, type=str)
+            ],
+            child_providers=[
+                ChildProvider(GrandSon, dest='GS')
+            ]
+        )
+
+    def __init__(self, params: Namespace) -> None:
+        print(params.hoge, params.huga)
+        gson = GrandSon(params._get_child('GS'))
+
+
+if __name__ == '__main__':
+    # set a root argument provider (same as other argument providers)
+    args_provider = ArgsProvider(
+        args=[Arg('foo', 'bar')],
+        child_providers=[ChildProvider(Son)]
+    )
+
+    # quite usual argparse way except *new code* line
+    parser = ArgumentParser()
+    parser.add_argument('-V', '--version', action='version', version='v1.0')
+    args_provider.add_arguments(parser)  # *new code*
+    params = parser.parse_args()
+
+    # now you have ALL parameters including child and grandchild arguments
+    # please try to execute with --help
+    print(params.foo)
+    son = Son(params._get_child('Son'))

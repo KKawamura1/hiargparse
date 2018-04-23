6b99d88485747737f3c7ccc93bb4439a0090992e
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2018-04-23 10:25:37 +0900

    exampleを大幅に変更、configure操作はparserの仕事にした

--- examples/example_write_and_read.py
+++ examples/example_write_and_read.py
@@ -1,9 +1,47 @@
 from hiargparse import ArgumentParser, Namespace
 from hiargparse import ArgsProvider, Arg, ChildProvider, ConfigureFileType
 from example import Son
-from typing import Optional
+
+import argparse
+from typing import Optional, Any
 from pathlib import Path
 from sys import exit
+
+
+# write file action
+class WriteToFileAction(argparse.Action):
+    def __call__(
+            self,
+            parser: argparse.ArgumentParser,
+            namespace: argparse.Namespace,
+            values: Any,
+            option_string: str = None
+    ) -> None:
+        assert isinstance(parser, ArgumentParser)
+        assert isinstance(namespace, Namespace)
+        assert isinstance(values, Path)
+        assert 'file_type' in namespace and namespace.file_type is not None
+        # write configure arguments to the given file as the given type
+        file_type = ConfigureFileType[namespace.file_type]
+        parser.write_configure_arguments(path=values, file_type=file_type)
+        exit()
+
+
+# read file action
+class ReadFromFileAction(argparse.Action):
+    def __call__(
+            self,
+            parser: argparse.ArgumentParser,
+            namespace: argparse.Namespace,
+            values: Any,
+            option_string: str = None
+    ) -> None:
+        assert isinstance(parser, ArgumentParser)
+        assert isinstance(namespace, Namespace)
+        assert isinstance(values, Path)
+        # read configure arguments from the given file
+        new_params = parser.read_configure_arguments(path=values)
+        namespace._update(new_params)
 
 
 if __name__ == '__main__':
@@ -11,29 +49,22 @@ if __name__ == '__main__':
     file_type_names = [file_type.name for file_type in ConfigureFileType]
     args_provider = ArgsProvider(
         args=[
-            Arg('write-to', type=Path),
-            Arg('read-from', type=Path),
-            Arg('file-type', default='json', choices=file_type_names)
+            Arg('file-type', default='json', choices=file_type_names),
+            # write-to argument
+            Arg('write-to', type=Path, action=WriteToFileAction,
+                help='%(default-text)s Write a configure file in the given path and exit. '),
+            # read-from argument
+            Arg('read-from', type=Path, action=ReadFromFileAction,
+                help='%(default-text)s Read from the given configure file. '),
         ],
         child_providers=[ChildProvider(Son)]
     )
 
     # parse arguments as usual
     parser = ArgumentParser()
+    parser.add_argument('-V', '--version', action='version', version='v1.0')
     args_provider.add_arguments(parser)
     params = parser.parse_args()
-
-    write_to: Optional[Path] = params.write_to
-    read_from: Optional[Path] = params.read_from
-    file_type: ConfigureFileType = ConfigureFileType[params.file_type]
-    if write_to is not None:
-        # write configure arguments to the given file
-        args_provider.write_configure_arguments(path=write_to, file_type=file_type)
-        exit()
-    if read_from is not None:
-        # read configure arguments from the given file
-        read_params = args_provider.read_configure_arguments(path=read_from)
-        params._update(read_params)
 
     # now you have ALL parameters including child and grandchild arguments
     # please execute with --write-to FILE_PATH

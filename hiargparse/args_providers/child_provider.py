a6ea88a128ae19b87d125a72c17a090731c7d303
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2018-04-26 12:09:31 +0900

    add comments to child_provider

--- hiargparse/args_providers/child_provider.py
+++ hiargparse/args_providers/child_provider.py
@@ -1,5 +1,6 @@
 from typing import Type, AbstractSet, TYPE_CHECKING
 from typing_extensions import Protocol
+from hiargparse.miscs import if_none_then
 from .exceptions import ArgumentError
 
 # avoid cyclic importing
@@ -16,14 +17,17 @@ class SupportsArgsProvider(Protocol):
 
 
 class ChildProvider:
+    """Struct that represents a child ArgsProvider."""
+
     def __init__(
             self,
             cls: Type[SupportsArgsProvider],
             name: str = None,
             dest: str = None,
             prefix: str = None,
-            no_provides: AbstractSet[str] = set()
+            no_provides: AbstractSet[str] = None
     ) -> None:
+        no_provides = if_none_then(no_provides, set())
         if name is None:
             name = cls.__name__
             assert name is not None

e5ab1933bef89a5d18c4dfadd05242ba6b0e6143
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2018-04-26 15:51:02 +0900

    set __all__

--- hiargparse/__init__.py
+++ hiargparse/__init__.py
@@ -2,3 +2,10 @@ from hiargparse.alternatives import Namespace, ArgumentParser
 from hiargparse.file_protocols import ConfigureFileType
 from hiargparse.args_providers import ArgsProvider, Arg, ChildProvider
 from hiargparse.args_providers import ArgumentError, ConflictWarning, PropagationError, ConflictError
+
+__all__ = [
+    'Namespace', 'ArgumentParser',
+    'ConfigureFileType',
+    'ArgsProvider', 'Arg', 'ChildProvider',
+    'ArgumentError', 'ConflictWarning', 'PropagationError', 'ConflictError'
+]
